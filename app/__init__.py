0207b13d534bab9ed379fb669785027b9c7ab751
mutazircodes <mutazircodes@gmail.com>
2024-12-19 13:12:12 +0530

    The database initialization has been sperated from app creation

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from .config import Config
-
-db = SQLAlchemy()
-migrate = Migrate()
+from app.utils.db import db, migrate
+from app.config import Config
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(Config)  
+
     db.init_app(app)
     migrate.init_app(app, db)
+
     return app
