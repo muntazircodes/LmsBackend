fada1ba7b09bdcf9d56fb46a31cfcb0d9b52d729
mutazircodes <mutazircodes@gmail.com>
2024-12-31 14:24:13 +0530

    Initialize JWT integration in Flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,23 @@
 from flask import Flask
+from app.utils.token import jwt
 from app.utils.db import db, migrate
 from app.config import Config
 
+
 def create_app():
-    """Application factory to initialize the Flask app."""
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
     migrate.init_app(app, db)
+    jwt.init_app(app)
 
     with app.app_context():
+
         from app.models.book_model import Books, Copies, Borrowing, Reserve
         from app.models.libraries_model import Libraries, Location
         from app.models.user_model import User, Report
 
     return app
-
 
 __all__ = ["Libraries", "Books", "Copies", "Borrowing", "Reserve", "Location", "User", "Report"]
