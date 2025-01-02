392ba564f1db0b04764546365d167e19267229f4
mutazircodes <mutazircodes@gmail.com>
2025-01-02 14:14:52 +0530

    Refactor RacksRepository to streamline rack retrieval methods; add is_admin method in UserRepository for user role verification

--- app/repositories/library_repository.py
+++ app/repositories/library_repository.py
@@ -72,6 +72,7 @@ class LibraryRepository:
 
 
 class RacksRepository:
+
     @staticmethod
     def get_rack_by_id(rack_id):
         return Racks.query.get(rack_id)
@@ -127,7 +128,6 @@ class RacksRepository:
             db.session.rollback()
             raise e
 
-class RacksRepository:
     @staticmethod
     def get_rack_by_id(rack_id):
         return Racks.query.get(rack_id)
@@ -137,48 +137,24 @@ class RacksRepository:
         return Racks.query.filter_by(lib_id=lib_id).all()
     
     @staticmethod
-    def add_rack(lib_id, block, floor, room, locker, rack_no):
-        try:
-            new_rack = Racks(
-                lib_id=lib_id,
-                block=block,
-                floor=floor,
-                room=room,
-                locker=locker,
-                rack_no=rack_no
-            )
-            db.session.add(new_rack)
-            db.session.commit()
-            return new_rack
-        except Exception as e:
-            db.session.rollback()
-            raise e
-
+    def get_rack_by_block(lib_id, block):
+        return Racks.query.filter_by(lib_id=lib_id, block=block).all()
+    
     @staticmethod
-    def update_rack(rack_id, block, floor, room, locker, rack_no):
-        try:
-            rack = Racks.query.get(rack_id)
-            if not rack:
-                raise ValueError("Racks not found")
-            rack.block = block
-            rack.floor = floor
-            rack.room = room
-            rack.locker = locker
-            rack.rack_no = rack_no
-            db.session.commit()
-            return rack
-        except Exception as e:
-            db.session.rollback()
-            raise e
-
+    def get_rack_by_floor(lib_id, floor):
+        return Racks.query.filter_by(lib_id=lib_id, floor=floor).all()
+    
     @staticmethod
-    def delete_rack(rack_id):
-        try:
-            rack = Racks.query.get(rack_id)
-            if not rack:
-                raise ValueError("Racks not found")
-            db.session.delete(rack)
-            db.session.commit()
-        except Exception as e:
-            db.session.rollback()
-            raise e
+    def get_rack_by_room(lib_id, room):
+        return Racks.query.filter_by(lib_id=lib_id, room=room).all()
+    
+    @staticmethod
+    def get_rack_by_locker(lib_id, locker):
+        return Racks.query.filter_by(lib_id=lib_id, locker=locker).all()
+    
+    @staticmethod
+    def get_rack_by_rack_no(lib_id, rack_no):
+        return Racks.query.filter_by(lib_id=lib_id, rack_no=rack_no).all()
+    
+    def get_books_in_rack(rack_id):
+        return Racks.query.get(rack_id).books
