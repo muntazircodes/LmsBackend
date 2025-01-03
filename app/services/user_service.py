e9633324d50e420fa2916f96b716d6fd030b354f
mutazircodes <mutazircodes@gmail.com>
2025-01-03 21:18:03 +0530

    Refactor UserService and AdminService to remove validation methods; streamline error handling and data processing in user and library registration.

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -5,14 +5,6 @@ from app.repositories.user_repository import UserRepository
 
 
 class UserService:
-
-    @staticmethod
-    def validate_and_serialize(data, validators):
-        for field, validator in validators.items():
-            if not validator(data.get(field)):
-                return Responses.validation_error({field: f"Invalid {field.replace('user_', '').replace('_', ' ')} format"})
-        return None
-
     @staticmethod
     def handle_repository_action(action, *args, **kwargs):
         try:
@@ -20,7 +12,7 @@ class UserService:
             if not result:
                 return Responses.not_found(action.__name__.split('_')[1].capitalize())
             return Validators.serialize_model(result) if not isinstance(result, list) else [Validators.serialize_model(item) for item in result]
-        except Exception as e:
+        except Exception:
             return Responses.server_error()
 
     @staticmethod
@@ -29,19 +21,15 @@ class UserService:
 
     @staticmethod
     def update_my_profile(user_id, user_data):
-        validators = {
-            'user_name': Validators.validate_name,
-            'user_email': Validators.validate_email,
-            'user_password': Validators.validate_password,
-            'phone_number': Validators.validate_phone,
-            'profile_picture': Validators.validate_image
-        }
-
-        validation_error = UserService.validate_and_serialize(user_data, validators)
-        if validation_error:
-            return validation_error
-
-        return UserService.handle_repository_action(UserRepository.update_user, user_id, **user_data)
+        try:
+            allowed_fields = [
+                'user_name', 'user_email', 'user_password',
+                'phone_number', 'profile_picture'
+            ]
+            update_data = {field: user_data.get(field) for field in allowed_fields}
+            return UserService.handle_repository_action(UserRepository.update_user, user_id, **update_data)
+        except Exception:
+            return Responses.server_error()
 
     @staticmethod
     def check_my_borrowings(user_id):
@@ -53,31 +41,19 @@ class UserService:
 
     @staticmethod
     def change_my_password(user_id, old_password, new_password):
-        user = UserRepository.get_user_by_id(user_id)
-        if not user:
-            return Responses.not_found("User")
-
-        if not Validators.verify_password(old_password, user.get('user_password')):
-            return Responses.validation_error({"password": "Invalid password"})
-
-        if not Validators.validate_password(new_password):
-            return Responses.validation_error({"password": "Invalid password"})
-
-        new_password = Validators.hash_password(new_password)
-        return UserService.handle_repository_action(UserRepository.update_password, user_id, new_password)
+        try:
+            user = UserRepository.get_user_by_id(user_id)
+            if not user:
+                return Responses.not_found("User")
+            return UserService.handle_repository_action(UserRepository.update_password, user_id, new_password)
+        except Exception:
+            return Responses.server_error()
 
     @staticmethod
     def report_for_query(user_id, report_data):
-        validators = {
-            'subject': Validators.validate_name,
-            'message': Validators.validate_text,
-            'handled_by': Validators.validate_name
-        }
-
-        validation_error = UserService.validate_and_serialize(report_data, validators)
-        if validation_error:
-            return validation_error
-
-        report_data['handled'] = report_data.get('handled', False)
-        report_data['report_date'] = datetime.now()
-        return UserService.handle_repository_action(UserRepository.add_report, user_id, **report_data)
+        try:
+            report_data['handled'] = report_data.get('handled', False)
+            report_data['report_date'] = datetime.now()
+            return UserService.handle_repository_action(UserRepository.add_report, user_id, **report_data)
+        except Exception:
+            return Responses.server_error()
